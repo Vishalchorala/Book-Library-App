a1cac5abf1bca907559505f2517160a04bc1b79e
Vishal Chorala <vishalchorala.dreamspot@gmail.com>
2025-05-14 17:24:12 +0530

    change

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -44,7 +44,7 @@ const Profile = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
+    <div className="flex flex-col items-center justify-center w-full h-full m-3">
       <motion.div
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
